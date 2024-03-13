4c938e3194386edc5c4d7fa9d9dfedf2497710b6
Chris Williams <orktagonapus@gmail.com>
2024-03-13 20:48:14 +0000

    Cat Eq implementation

--- app/src/main/scala/scalawithcats/exercises/App.scala
+++ app/src/main/scala/scalawithcats/exercises/App.scala
@@ -10,17 +10,27 @@ final case class Cat(name: String, age: Int, colour: String)
 
 object App {
   def main(args: Array[String]): Unit = {
-    implicit val catShow: Show[Cat] =
-      new Show[Cat] {
-        def show(c: Cat): String = s"${c.name} is a ${c.age.toString} year-old ${c.colour} cat."
-      }
+    implicit val catEq: Eq[Cat] = Eq.instance[Cat] {
+      (cat1, cat2) =>
+        cat1.name === cat2.name && cat1.age === cat2.age && cat1.colour === cat2.colour
+    }
 
-    val cat = Cat(
+    val cat1 = Cat(
       name = "Sherlock",
       age = 3,
       colour = "black"
     )
-    println(cat.show)
+    val cat2 = Cat(
+      name = "Garfield",
+      age = 25,
+      colour = "orange"
+    )
+
+    val optionCat1 = Option(cat1)
+    val optionCat2 = Option.empty[Cat]
+
+    println(cat1 === cat2)
+    println(optionCat1 === optionCat2)
   }
 
 }
